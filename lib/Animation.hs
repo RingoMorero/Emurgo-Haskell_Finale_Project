b1b11bbe9a029cc042c2a8a72bb02a39206029e2
RingoMorero <69268786+RingoMorero@users.noreply.github.com>
2022-06-27 15:50:53 -0300

    Create Animation.hs

---
+++ lib/Animation.hs
@@ -0,0 +1,11 @@
+module Animation
+    ( module Animation.Env
+    , module Animation.Render
+    , module Animation.State
+    , module Animation.Type
+    ) where
+
+import Animation.Env        -- | IT DOES NOT REQUIRE COMMENTS. CODE IS CLEAR ENOUGH
+import Animation.Render     -- | IT DOES NOT REQUIRE COMMENTS. CODE IS CLEAR ENOUGH
+import Animation.State      -- | IT DOES NOT REQUIRE COMMENTS. CODE IS CLEAR ENOUGH
+import Animation.Type       -- | IT DOES NOT REQUIRE COMMENTS. CODE IS CLEAR ENOUGH
